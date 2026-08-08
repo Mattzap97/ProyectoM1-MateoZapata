7f37bd395835463b8378fdae92262e894c75168c
Mattzap97 <mattalexxz@gmail.com>
2026-08-08 18:22:22 -0500

    Crear y vincular un archivo de JavaScript

--- README.md
+++
@@ -1,2 +0,0 @@
-# ProyectoM1-MateoZapata
-Página web que genera una paleta de colores en formatos HSL y HEX
---
+++ script/script.js
@@ -0,0 +1,9 @@
+const generateButton = document.getElementById("generate-btn");
+
+function generatePalette(){
+
+};
+generateButton.addEventListener("click", generatePalette {
+    console.log("Botón presionado!");
+
+});
