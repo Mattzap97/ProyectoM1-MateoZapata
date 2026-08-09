fa61e5d640f0747730e10d0ea6537ee2feab43eb
Mattzap97 <mattalexxz@gmail.com>
2026-08-08 22:03:06 -0500

    Salida de información de cada color en su tarjeta respectiva

--- script/script.js
+++ script/script.js
@@ -1,21 +1,22 @@
 const colors = document.querySelectorAll(".color");
 const generateButton = document.getElementById("generate-btn");
 
-function generatePalette(){
+function generatePalette() {
     const randomColor = Math.floor(Math.random() * 16777216);
     const hexColor = "#" + randomColor.toString(16).padStart(6, "0");
 
     return hexColor;
 }
 
-generateButton.addEventListener("click", function(){
-
+generateButton.addEventListener("click", function() {
     console.log("Botón presionado!");
+
     colors.forEach(function(color) {
     const newColor = generatePalette();
     color.style.backgroundColor = newColor;
+    color.textContent = newColor;
 
-    })    
+    })  
 })
 
 const copyButton = document.getElementById("copy-btn");
