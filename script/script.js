99df95d8d17d9eae2d269d72aa4a999d9f98d246
Mattzap97 <mattalexxz@gmail.com>
2026-08-08 19:01:28 -0500

    Agregar función de generar paleta

--- script/script.js
+++ script/script.js
@@ -1,8 +1,15 @@
 const generateButton = document.getElementById("generate-btn");
+
 function generatePalette(){
-    console.log("Botón presionado!");
+    const randomColor = Math.floor(Math.random() * 16777216);
+    const hexColor = "#" + randomColor.toString(16).padStart(6, "0");
+
+    return hexColor;
 }
-generateButton.addEventListener("click", generatePalette);
+console.log(generatePalette());
+generateButton.addEventListener("click", function(){
+    console.log("Botón presionado!");
+})
     
 
 const copyButton = document.getElementById("copy-btn");
