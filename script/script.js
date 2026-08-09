ef51b09c12da5edaad3eec62fd8ff4030ed658fc
Mattzap97 <mattalexxz@gmail.com>
2026-08-08 23:25:46 -0500

    Función agregada en javascript para mostrar la paleta de colores al abrir el archivo .html

--- script/script.js
+++ script/script.js
@@ -1,23 +1,30 @@
-const colors = document.querySelectorAll(".color");
+const colorItems = document.querySelectorAll(".color-item");
 const generateButton = document.getElementById("generate-btn");
 
 function generatePalette() {
     const randomColor = Math.floor(Math.random() * 16777216);
     const hexColor = "#" + randomColor.toString(16).padStart(6, "0");
-
     return hexColor;
 }
 
-generateButton.addEventListener("click", function() {
-    console.log("Botón presionado!");
-
-    colors.forEach(function(color) {
-    const newColor = generatePalette();
-    color.style.backgroundColor = newColor;
-    color.textContent = newColor;
+function paintPalette() {
+    colorItems.forEach(function(item) {
+        const newColor = generatePalette();
+        const box = item.querySelector(".color");
+        const name = item.querySelector(".name");
+        box.style.backgroundColor = newColor;
+        name.textContent = newColor;
 
     })  
+}
+
+paintPalette();
+
+generateButton.addEventListener("click", function() {
+    console.log("Botón presionado!");
+    paintPalette();
 })
+
 
 const copyButton = document.getElementById("copy-btn");
 function copyPalette(){
